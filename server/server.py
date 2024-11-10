da30b401335a891239f8e95a7bd49b3452d0e1f1
Bruno-BRG <brg.brunorocha@gmail.com>
2024-11-10 19:02:54 -0300

    Update image handling and filtering in dashboard: modify server to list PNG files, enhance HTML for residue type filtering, and improve JavaScript functionality for dynamic image display.

--- server/server.py
+++ server/server.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    image_folder = os.path.join('static', 'Graphs')
-    images = [f for f in os.listdir(image_folder) if f.endswith('.png') or f.endswith('.jpg')]
-    return render_template('index.html', images=images)
+    files = [f for f in os.listdir('/home/bubu/Documents/Descartin/server/static/Graphs') if f.endswith('.png')]
+    return render_template('index.html', files=files)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True)
 
--- server/static/script.js
+++ server/static/script.js
@@ -3,3 +3,17 @@ function updateImage() {
     var image = document.getElementById("displayedImage");
     image.src = select.value;
 }
+
+// script.js
+function filterImages() {
+  const filter = document.getElementById('residue-type').value.toLowerCase();
+  const images = document.querySelectorAll('.residue-image');
+
+  images.forEach(image => {
+    if (filter === 'all' || image.alt.toLowerCase() === filter) {
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  });
+}
