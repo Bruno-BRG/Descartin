57111db973e144a23a4ddaafcfbeeeda825a6576
Bruno-BRG <brg.brunorocha@gmail.com>
2025-01-22 09:10:39 -0300

    Remove password authentication logic from Streamlit app

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,44 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-API_URL = "http://0.0.0.0:8000"  # Use the service name 'api' instead of 'localhost'
-
-def check_password():
-    """Returns `True` if the user had a correct password."""
-
-    def login_form():
-        """Form with widgets to collect user information"""
-        with st.form("Credentials"):
-            st.text_input("Usuario", key="username")
-            st.text_input("Senha", type="password", key="password")
-            st.form_submit_button("Log in", on_click=password_entered)
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["username"] in st.secrets[
-            "passwords"
-        ] and hmac.compare_digest(
-            st.session_state["password"],
-            st.secrets.passwords[st.session_state["username"]],
-        ):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the username or password.
-            del st.session_state["username"]
-        else:
-            st.session_state["password_correct"] = False
-
-    # Return True if the username + password is validated.
-    if st.session_state.get("password_correct", False):
-        return True
-
-    # Show inputs for username + password.
-    login_form()
-    if "password_correct" in st.session_state:
-        st.error("😕 Senha ou usuario desconhecido")
-    return False
-
-if not check_password():
-    st.stop()  # Do not continue if check_password is not True.
+API_URL = "http://192.81.214.49:8000"  # Use the service name 'api' instead of 'localhost'
 
 # Main Streamlit app starts here
 def fetch_data():
